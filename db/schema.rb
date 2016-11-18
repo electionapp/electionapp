8de6a4a764c1f6848888ff657ac20c79235a0b8f
Evan Carlin <ecarlin@pugetsound.edu>
2016-11-17 18:48:00 -0800

    Removed extra end from routes

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20161118012730) do
 
   create_table "candidates", force: :cascade do |t|
