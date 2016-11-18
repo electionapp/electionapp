8faa9d99ced20d4a11b42434d3696d002b467c21
Alexander Harris <abharris@pugetsound.edu>
2016-11-17 17:43:25 -0800

    destory vote modle & table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118012730) do
-
-  create_table "candidates", force: :cascade do |t|
-    t.string   "name"
-    t.text     "biography"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20161115074715) do
 
   create_table "elections", force: :cascade do |t|
     t.text     "test"
