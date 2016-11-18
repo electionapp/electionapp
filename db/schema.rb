343fa33d7fb728f10c8a24ccfa262122080a01e5
Alexander Harris <abharris@pugetsound.edu>
2016-11-17 17:54:43 -0800

    remove vote table from schema

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20161115074715) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.integer  "position_id"
+    t.integer  "candidate_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
