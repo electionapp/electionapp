e3741fc1526ec19b5b1245248329c5154bf5ea5f
AlexTomes <alex.t.95@gmail.com>
2016-11-17 17:56:46 -0800

    Seeding for election permissions & deletion of bug in index. Elections_table refined.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Position.create(title: "seed", description: "alex's seed", election_id: 1,)
