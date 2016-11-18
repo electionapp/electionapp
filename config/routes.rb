007848cf6ce13917ee052cf6ce5a7297b2684c44
Alexander Harris <abharris@pugetsound.edu>
2016-11-17 17:30:13 -0800

    $ generate scaffold for candidates & reroute candidates

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   resources :elections do
     resources :positions
     resources :users
+    resources :candidates
   end
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
