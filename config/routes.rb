aaf37ec097ae7c68d846754ebc8160d6ab974297
Evan Carlin <ecarlin@pugetsound.edu>
2016-11-22 12:32:12 -0800

    Cleaned up routing file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
+resources :users do
+  resources :elections do
+    resources :positions do
+      resources :candidates
+    end
+  end
+end
+
   #****** CLEARANCE routes that have been redefined for our project structure  ******
   # Probably don't need to touch these
   resources :passwords, controller: "passwords", only: [:create, :new]
@@ -14,60 +24,4 @@ Rails.application.routes.draw do
   delete "/sign_out" => "sessions#destroy", as: "sign_out"
   get "/sign_up" => "users#new", as: "sign_up"
   #************************************************************
-
-# ********* Ordering matters in this file ********
-# base = "/user/:id/elections"
-# #Election routes
-# get "#{base}/new" => "elections#new", as: "new_election"
-# post "#{base}/new" => "elections#create", as: "submit_election"
-# get "#{base}" => "elections#index", as: "show_elections"
-# get "#{base}/:id" => "elections#show", as: "election"
-# get "/#{base}/:id/edit" => "elections#edit", as: "edit_election"
-# post "#{base}/:id/edit" => "elections#update", as: "update_election"
-
-# get "#{base}/:id/positions" => "positions#index", as: "show_positions"
-# get "#{base}/:id/postions/new" => "positions#new", as: "new_position"
-# post "#{base}/:id/positions/new" => "positions#create", as: "submit_position"
-#  # resource :positions
-#   resource :candidates
-resources :users do
-  resources :elections do
-    resources :positions do
-      resources :candidates
-    end
-  end
-end
-
-
-  # resources :positions do
-  #   resources :candidates
-  # end
-
-  # resource :elections
-  # resource :elections do
-  #     get "add_users"
-  # end
-
-  # resources :positions do
-  #   member do
-  #     post 'upvote'
-  #   end
-  # end
-
-  # resources :elections do
-  #   resources :positions
-  # end
-
-  # resources :users do
-  #   resources :elections
-  # end
-
-
-  # resources :users
-  # resources :candidates
-
-
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pages, only: [:new]
 end
