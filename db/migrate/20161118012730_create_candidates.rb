5fc26a33251a6db3ad65f98fcd786df83545091c
Evan Carlin <ecarlin@pugetsound.edu>
2016-11-22 10:02:17 -0800

    Finalized candidates to postions

--- db/migrate/20161118012730_create_candidates.rb
+++ db/migrate/20161118012730_create_candidates.rb
@@ -1,8 +1,9 @@
 class CreateCandidates < ActiveRecord::Migration[5.0]
   def change
     create_table :candidates do |t|
-      t.string :name
-      t.text :biography
+    	t.belongs_to :position, index: true, null: false
+    	t.string :name
+    	t.text :biography
 
       t.timestamps
     end
