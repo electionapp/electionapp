1112db6683114fe479957bbb6f33a4d49e77e692
Alexander Harris <abharris@pugetsound.edu>
2016-11-27 22:05:32 -0800

    define upvote function in app/controllers/topics_controller.rb to allow users to vote

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -62,6 +62,10 @@ class CandidatesController < ApplicationController
     end
   end
 
+  def upvote
+
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_candidate
