b186a0ac1d215a16eb305bfcdb36e1ce1eb278fd
AlexTomes <alex.t.95@gmail.com>
2016-12-04 19:56:56 -0800

    user delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,16 @@ class UsersController < Clearance::UsersController
 		super
 	end
 
+def show
+end
+
+def destroy
+	User.find(params[:id]).destroy
+	redirect_to root_path
+end
+
 	def elections
 		@User.elections
 	end
-	
+
 end
