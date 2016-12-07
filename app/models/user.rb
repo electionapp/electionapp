bf909e6a8f2bf3481a81a0883bc5cc20c307406e
Evan Carlin <ecarlin@pugetsound.edu>
2016-12-07 12:34:32 -0800

    Removed update timestamp for election and added email validation, Email stuff COMPLETE!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   include Clearance::User
-  #validates :email, :format => { :with => /@pugetsound.edu/, :message => "must end in @pugetsound.edu" }
+  validates :email, :format => { :with => /@pugetsound.edu/, :message => "must end in @pugetsound.edu" }
   has_and_belongs_to_many :elections
 end
